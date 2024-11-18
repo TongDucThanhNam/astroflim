0361e7ff8bf12f0d4ed721aa4c8944be15695665
tongducthanhnam <tongducthanhnam@gmail.com>
2024-11-19 01:11:28 +0700

    add try catch for server island

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,10 +15,10 @@ export default defineConfig({
     output: "server",
     adapter: vercel(
         {
-            isr: {
-                expiration: 60 * 60 * 24,
-                exclude: ['/api/**'],
-            },
+            // isr: {
+            //     expiration: 60 * 60 * 24,
+            //     exclude: ['/api/**'],
+            // },
             imagesConfig: {
                 domains: ["wsrv.nl"],
                 sizes: [320, 640, 768, 1024, 1280, 1536, 1920],
